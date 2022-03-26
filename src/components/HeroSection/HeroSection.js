3bac68de0022ebaff72ace3f65a5e6c40ab24aca
Richard Smith <rsmith@finixcreative.com>
2022-03-26 00:29:58 -0700

    Hero slides

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import styles from './HeroSection.module.css'
 
-const HeroSection = () => (
+const HeroSection = (props) => (
   <div className={styles.HeroSection}>
-    HeroSection Component
+    <h1>HeroSection Component</h1>
+    {Object.keys(props.Slides).map(slide => (
+      <div key={slide}>
+        <img src={props.Slides[slide].image} />
+        <p>{props.Slides[slide].title}</p>
+      </div>
+    ))}
   </div>
 )
 
---
+++ src/components/HeroSection/Slides.js
@@ -0,0 +1,27 @@
+const heroSlides = [
+	{
+		title: 'Shop New Arrivals',
+		subtitle: 'Our coolest new items are waiting for you!',
+		image: 'Images/hero-1.png'
+	},
+	{
+		title: 'Our Fave Tees',
+		subtitle: 'Shop all of our favorites.',
+		image: 'Images/hero-2.png'
+	},
+	{
+		title: "Men's Tees",
+		subtitle: 'Find the perfect shirt.',
+		image: 'Images/hero-3.png'
+	}
+]
+
+const heroStatic = [
+	{
+		title: 'Our Favorite Tees',
+		subtitle: 'Everyday tees you need.',
+		image: '../Images/hero-4.png'
+	}
+]
+
+export { heroSlides, heroStatic }
