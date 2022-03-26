885926c8dec53daee5f2887f4d851ccde6b94af4
Richard Smith <rsmith@finixcreative.com>
2022-03-26 01:06:53 -0700

    Hero slide counter and control buttons

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './HeroSection.module.css'
 
-const HeroSection = (props) => (
-  <div className={styles.HeroSection}>
-    <h1>HeroSection Component</h1>
-    {Object.keys(props.Slides).map(slide => (
-      <div key={slide}>
-        <img src={props.Slides[slide].image} />
-        <p>{props.Slides[slide].title}</p>
-      </div>
-    ))}
-  </div>
-)
+const HeroSection = (props) => {
+  const [SlideCounter, setSlideCounter] = useState(0)
+  const finalSlide = props.Slides.length
+
+  const prevSlide = () => {
+    SlideCounter > 0 ? setSlideCounter(SlideCounter - 1) : setSlideCounter(finalSlide)
+    console.log(SlideCounter)
+  }
+
+  const nextSlide = () => {
+    SlideCounter < finalSlide ? setSlideCounter(SlideCounter + 1) : setSlideCounter(0)
+    console.log(SlideCounter)
+  }
+
+  return (
+    <div className={styles.HeroSection}>
+      {Object.keys(props.Slides).map(slide => (
+        <div className={styles.Slide} key={slide}>
+          <img src={props.Slides[slide].image} />
+          <h1>{props.Slides[slide].title}</h1>
+          <p>{props.Slides[slide].subtitle}</p>
+          <a onClick={prevSlide}>Previous</a>
+          <a onClick={nextSlide}>Next</a>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export default HeroSection
