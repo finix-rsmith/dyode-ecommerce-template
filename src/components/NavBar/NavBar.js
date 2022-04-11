8d9b168da2a070c7771a8ca9a09f77d7543382b3
Richard Smith <rsmith@finixcreative.com>
2022-04-11 06:47:00 -0700

    Nav components

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import styles from './NavBar.module.css'
+import BrandLogo from '../BrandLogo/BrandLogo.js'
+import NavLinks from '../NavLinks/NavLinks.js'
+import { mainNav, Icons, navUtilities } from '../../SiteMap.js'
 
-const NavBar = () => (
+const NavBar = (props) => (
   <div className={styles.NavBar}>
-    NavBar Component
+    <BrandLogo Image={props.Logo} />
+    <div className={styles.NavMenu}>
+      <NavLinks Links={mainNav} Mobile={Icons.Nav} />
+    </div>
+    <div className={styles.NavUtility}>
+      <NavLinks Links={navUtilities} />
+    </div>
   </div>
 )
 
