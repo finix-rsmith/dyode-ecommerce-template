a4dfbad48acb3b82c9665f0109473c3d990392e6
Richard Smith <rsmith@finixcreative.com>
2022-04-11 06:48:49 -0700

    Image assets

---
+++ src/images/icons-import-map.js
@@ -0,0 +1,17 @@
+import Facebook from './icon-facebook.png'
+import Instagram from './icon-instagram.png'
+import Search from './icon-search.png'
+import Profile from './icon-profile.png'
+import Cart from './icon-cart.png'
+import Nav from './icon-nav.png'
+
+const Icons = {
+	Facebook,
+	Instagram,
+	Search,
+	Profile,
+	Cart,
+	Nav
+}
+
+export default Icons
